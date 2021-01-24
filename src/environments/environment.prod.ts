3ec6812ea17dc396bebd1b3246331ba96e906e61
ahmedkhan1 <ahmed_rises@hotmail.com>
2021-01-24 15:03:02 +0500

    Url Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     hmr       : false,
-    apiURL    : "https://motorlogs1.azurewebsites.net/api/",
+    apiURL    : "https://motorlogs2.azurewebsites.net/api/",
     apiURL2    : "http://localhost:5000/api/",
     sessionTimeout : 6000,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     hmr       : false,
-    apiURL    : "https://motorlogs1.azurewebsites.net/api/",
+    apiURL    : "https://motorlogs2.azurewebsites.net/api/",
     apiURL2    : "http://localhost:5000/api/",
     sessionTimeout : 6000,
     ARRIVING_SITE_VALUE: 'LENNOCK MOTORS'
