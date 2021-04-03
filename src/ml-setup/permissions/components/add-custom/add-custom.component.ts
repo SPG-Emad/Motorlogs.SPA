353cf18da4b4ee35eeb34a459783412d91ebabc8
Emad Khan <spidergrey.emad@gmail.com>
2021-04-04 02:57:32 +0500

    Permissions Add Custom Column Fixed

--- src/ml-setup/permissions/components/add-custom/add-custom.component.ts
+++ src/ml-setup/permissions/components/add-custom/add-custom.component.ts
@@ -1,19 +1,26 @@
-import { filter } from 'rxjs/operators';
-import { Component, OnInit, ChangeDetectionStrategy, ViewChild, Input, SimpleChanges, Inject } from '@angular/core';
-import { FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
-import { PermissionStateService } from 'ml-setup/shared/ngrx/service/permission-column-state.service';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { PermissionsService } from 'ml-setup/shared/services/permissions/permissions.service';
-import { ToastHandlerService } from 'app/shared/services/toast-handler.service';
+import { filter } from "rxjs/operators";
+import {
+    Component,
+    OnInit,
+    ChangeDetectionStrategy,
+    ViewChild,
+    Input,
+    SimpleChanges,
+    Inject,
+} from "@angular/core";
+import { FormGroup, Validators, FormBuilder, FormArray } from "@angular/forms";
+import { PermissionStateService } from "ml-setup/shared/ngrx/service/permission-column-state.service";
+import { MatDialog, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { PermissionsService } from "ml-setup/shared/services/permissions/permissions.service";
+import { ToastHandlerService } from "app/shared/services/toast-handler.service";
 
 @Component({
-    selector: 'ml-add-custom-column',
-    templateUrl: './add-custom.component.html',
-    styleUrls: ['./add-custom.component.scss'],
+    selector: "ml-add-custom-column",
+    templateUrl: "./add-custom.component.html",
+    styleUrls: ["./add-custom.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddCustomComponent implements OnInit {
-
     updateColumnID: number;
     modalTitle: string;
     colNameMsg = false;
@@ -24,105 +31,99 @@ export class AddCustomComponent implements OnInit {
         private toastHandlerService: ToastHandlerService,
         private permissionService: PermissionsService,
         private permissionStore: PermissionStateService,
-        @Inject(MAT_DIALOG_DATA) public modalParams?: any,
-
+        @Inject(MAT_DIALOG_DATA) public modalParams?: any
     ) {
         this.updateColumnID = null;
 
-        if (modalParams && modalParams.hasOwnProperty('key') && modalParams.key.colId) {
+        if (
+            modalParams &&
+            modalParams.hasOwnProperty("key") &&
+            modalParams.key.colId
+        ) {
             this.updateColumnID = modalParams.key.colId;
-
         }
-        this.modalTitle = (this.updateColumnID) ? "EDIT COLUMN" : "ADD COLUMN";
+        this.modalTitle = this.updateColumnID ? "EDIT COLUMN" : "ADD COLUMN";
     }
 
-
     /*Parent and Child Forms*/
-    @ViewChild('userRolesComp', { static: true }) userRolesComp;
-    @ViewChild('departmentComp', { static: true }) departmentComp;
-    @ViewChild('addColumn', { static: true }) addColumn;
+    @ViewChild("userRolesComp", { static: true }) userRolesComp;
+    @ViewChild("departmentComp", { static: true }) departmentComp;
+    @ViewChild("addColumn", { static: true }) addColumn;
 
     /*--------------------*/
-
 
     /*Setup Form*/
     setupForm: FormGroup = this.fb.group({
         colName: ["", [Validators.required]],
         columnWidth: ["", [Validators.required]],
         cellType: ["", [Validators.required]],
-        dropDownType: [{ value: '', disabled: true }, [Validators.required]],
+        dropDownType: [{ value: "", disabled: true }, [Validators.required]],
         options: this.fb.array([]),
     });
     /*-----------*/
 
-
     isEditable = false;
-    value = '100';
+    value = "100";
 
     fieldCounter = 0;
 
     dropDownType = [
         {
             id: 1,
-            name: 'Self-Learning',
-            code: 'DD-Self'
-
+            name: "Self-Learning",
+            code: "DD-Self",
         },
         {
             id: 2,
-            name: 'Suggested',
-            code: 'DD-Suggest'
+            name: "Suggested",
+            code: "DD-Suggest",
         },
         {
             id: 3,
-            name: 'Fixed',
-            code: 'DD-Fixed'
+            name: "Fixed",
+            code: "DD-Fixed",
         },
     ];
-
 
     cellTypes = [
         {
             id: 1,
-            name: 'Text'
+            name: "Text",
         },
         {
             id: 2,
-            name: 'Number'
+            name: "Number",
         },
         {
             id: 3,
-            name: 'Date'
+            name: "Date",
         },
         {
             id: 4,
-            name: 'Currency'
+            name: "Currency",
         },
         {
             id: 5,
-            name: 'Percentage'
+            name: "Percentage",
         },
         {
             id: 6,
-            name: 'Drop-down'
+            name: "Dropdown",
         },
         {
             id: 7,
-            name: 'Comments Box'
-        }
+            name: "Comments Box",
+        },
     ];
 
-
     ngOnInit() {
-
         const arr = this.setupFormOption;
         arr.controls = [];
 
-
         if (this.updateColumnID) {
             const obj = {
-                colName: 'Ahmed',
-                columnWidth: '150',
+                colName: "Ahmed",
+                columnWidth: "150",
                 cellType: 6,
                 dropDownType: 2,
                 options: [],
@@ -131,9 +132,9 @@ export class AddCustomComponent implements OnInit {
             this.value = obj.columnWidth;
 
             if (obj.cellType === 6) {
-                this.setupForm.get('dropDownType').enable();
+                this.setupForm.get("dropDownType").enable();
             } else {
-                this.setupForm.get('dropDownType').disable();
+                this.setupForm.get("dropDownType").disable();
             }
 
             this.setupForm.patchValue(obj);
@@ -143,11 +144,9 @@ export class AddCustomComponent implements OnInit {
         }
     }
 
-
     get setupFormOption() {
-        return this.setupForm.get('options') as FormArray;
+        return this.setupForm.get("options") as FormArray;
     }
-
 
     proceed() {
         this.colNameMsg = true;
@@ -159,21 +158,20 @@ export class AddCustomComponent implements OnInit {
         /*-----------------------------*/
 
         if (option === 6) {
-            this.setupForm.get('dropDownType').enable();
+            this.setupForm.get("dropDownType").enable();
         } else {
-            this.setupForm.get('dropDownType').disable();
+            this.setupForm.get("dropDownType").disable();
         }
     }
 
-
     onDropDownTypeSelect(event) {
         const option = event.value;
-        if (option === 'DD-Fixed') {
+        if (option === "DD-Fixed") {
             this.addOptions();
         } else {
             const arr = this.setupFormOption;
             arr.controls = [];
-            this.setupForm.get('options').setValue([]);
+            this.setupForm.get("options").setValue([]);
         }
     }
 
@@ -193,12 +191,10 @@ export class AddCustomComponent implements OnInit {
     addOptions() {
         const group = {};
 
-        let label = 'option' + this.fieldCounter;
-        group['option'] = ['', [Validators.required, Validators.maxLength(10)]];
+        let label = "option" + this.fieldCounter;
+        group["option"] = ["", [Validators.required, Validators.maxLength(10)]];
 
-        this.setupFormOption.push(
-            this.fb.group(group)
-        );
+        this.setupFormOption.push(this.fb.group(group));
         this.fieldCounter++;
     }
 
@@ -216,24 +212,26 @@ export class AddCustomComponent implements OnInit {
         let regex = /[a-zA-Z ]|\./;
         if (!regex.test(key)) {
             theEvent.returnValue = false;
-            if (theEvent.preventDefault) { theEvent.preventDefault(); }
+            if (theEvent.preventDefault) {
+                theEvent.preventDefault();
+            }
         }
     }
 
     getFormValues() {
         const options = [];
-        if (this.setupForm.get('dropDownType').value !== "") {
-            this.setupForm.get('dropDownType').value.map(res => {
+        if (this.setupForm.get("dropDownType").value !== "") {
+            this.setupForm.get("dropDownType").value.map((res) => {
                 console.log(res);
                 options.push(res);
             });
         }
 
         const obj = {
-            colName: this.setupForm.get('colName').value,
-            columnWidth: this.setupForm.get('columnWidth').value,
-            cellType: this.setupForm.get('cellType').value,
-            dropDownType: this.setupForm.get('dropDownType').value,
+            colName: this.setupForm.get("colName").value,
+            columnWidth: this.setupForm.get("columnWidth").value,
+            cellType: this.setupForm.get("cellType").value,
+            dropDownType: this.setupForm.get("dropDownType").value,
             options: this.fb.array([]),
         };
 
@@ -242,11 +240,11 @@ export class AddCustomComponent implements OnInit {
 
     createUpdateColumn() {
         /**
-         * 
-         * userRolesComp 
-         * departmentComp   
+         *
+         * userRolesComp
+         * departmentComp
          * setupForm
-         * 
+         *
          * */
         const resultObj = {
             ...this.setupForm.value,
@@ -264,35 +262,52 @@ export class AddCustomComponent implements OnInit {
     }
 
     createColumn(resultObj) {
-        this.permissionService.postPermission(resultObj)
-            .subscribe(
-                (res) => {
-                    /*Display success message*/
-                    this.toastHandlerService.generateToast(res.message, 'OK', 2000);
-                    /*-----------------------*/
-                    this.dialog.closeAll();
+        const obj = resultObj;
+        let cellName = "";
+        this.cellTypes
+            .filter((x) => x.id == obj.cellType)
+            .map((x) => {
+                cellName = x.name;
+            });
+        obj.cellType = cellName;
+        console.log(obj);
 
-                },
-                err => {
-                    this.toastHandlerService.generateToast('Unable to perform action' + err, 'OK', 2000);
-                }
-            );
+        this.permissionService.postPermission(resultObj).subscribe(
+            (res) => {
+                /*Display success message*/
+                this.toastHandlerService.generateToast(res.message, "OK", 2000);
+                /*-----------------------*/
+                this.dialog.closeAll();
+            },
+            (err) => {
+                this.toastHandlerService.generateToast(
+                    "Unable to perform action" + err,
+                    "OK",
+                    2000
+                );
+            }
+        );
     }
 
     updateColumn(resultObj) {
-        this.permissionService.postPermission(resultObj)
-            .subscribe(
-                (res) => {
-                    /*Display success message*/
-                    this.toastHandlerService.generateToast('Column updated successfully', 'OK', 2000);
-                    /*-----------------------*/
-                    this.dialog.closeAll();
-
-                },
-                err => {
-                    this.toastHandlerService.generateToast('Unable to perform action' + err, 'OK', 2000);
-                }
-            );
+        this.permissionService.postPermission(resultObj).subscribe(
+            (res) => {
+                /*Display success message*/
+                this.toastHandlerService.generateToast(
+                    "Column updated successfully",
+                    "OK",
+                    2000
+                );
+                /*-----------------------*/
+                this.dialog.closeAll();
+            },
+            (err) => {
+                this.toastHandlerService.generateToast(
+                    "Unable to perform action" + err,
+                    "OK",
+                    2000
+                );
+            }
+        );
     }
-
 }
