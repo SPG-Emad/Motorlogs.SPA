7c527431328893475b4a8bd7095e16c2798e2bb5
Emad Khan <spidergrey.emad@gmail.com>
2022-02-26 23:05:19 +0500

    Applied search filter in column options

--- src/ml-routine/saleslog/components/column-option/column-option.component.ts
+++ src/ml-routine/saleslog/components/column-option/column-option.component.ts
@@ -180,4 +180,9 @@ export class ColumnOptionComponent implements OnInit {
             });
         }
     }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
 }
